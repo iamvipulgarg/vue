c05f3aa4ead917e830ff553bf5096ad773c41509
Vipul Garg <vipulgarg024@gmail.com>
2025-05-10 11:53:27 +0530

    notes,install

---
+++ project1/p1/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Text from './components/Text.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="First Vue Project!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
+  <Text />
+  <h2>Vue Heading 2</h2>
+  <Text />
+  <Text />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+h2{
+  color:green;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ project1/p1/src/components/Text.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Heading</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quia quas aliquid quibusdam ratione laboriosam illum maiores, similique deleniti nisi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quia quas aliquid quibusdam ratione laboriosam illum maiores, similique deleniti nisi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quia quas aliquid quibusdam ratione laboriosam illum maiores, similique deleniti nisi?</p>
+
+    <ul>
+        <li>Lorem, ipsum.</li>
+        <li>Lorem, ipsum.</li>
+        <li>Lorem, ipsum.</li>
+    </ul>
+
+</template>
+  
+  <style scoped>
+  
+  *{
+    font-size: 20px;
+  }
+  h1{
+    color: blue;
+  }
+  p{
+    margin-top: 10px;
+  }
+
+  </style>
+  
